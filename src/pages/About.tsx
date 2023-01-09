f2cca436ad9045f3e8de296d66df163acf5faed6
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-01-09 21:59:02 +0900

    pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+const About = (): JSX.Element => {
+  return (
+    <>
+      <h1>About</h1>
+      <h3>Our History</h3>
+      <h3>Contact</h3>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = (): JSX.Element => {
+    return (
+        <>
+            <h1>Home</h1>
+            <a href="/store">Shop now?</a>
+        </>
+    )
+}
+
+export default Home
